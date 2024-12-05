438bcdc4fa19422e4b47033b7bbe8848d975d8ed
Revhieno Roll Haurly MBOUTA <haurlyroll@icloud.com>
2024-12-05 09:26:30 +0000

    annulation du dernier commit

--- messenger-frontend/src/api.js
+++ messenger-frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { io } from "socket.io-client";
 
 const api = axios.create({
-  baseURL: "https://ztext.onrender.com",
+  baseURL: "https://ztext.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
